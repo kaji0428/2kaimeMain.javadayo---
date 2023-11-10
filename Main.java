0e451efa20448dd55c5335a59d3d9b92ca5d4500
kaji <k.notsukitaira.sys23@morijyobi.ac.jp>
2023-11-10 11:30:25 +0900

    2人目できるようになったよー

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 public class Main{
 public static void main(String[] args) {
+	for(int i=1; i<3; i++){
 	Scanner sc = new Scanner(System.in);
-	System.out.println("身長(m)");
+	System.out.println(i + "の"+"身長(m)");
 	double height = sc.nextDouble();
-	System.out.println("体重(kg)");
+	System.out.println(i + "の"+"体重(kg)");
 	double weight = sc.nextDouble();
 	double bmi = 0.0;
 	bmi = weight / (height * height);
-	System.out.print("BMIの値は");
+	System.out.printi + "の"+("BMIの値は");
 	System.out.println((Math.floor(bmi * 10))/10);
+	}
 }
 
 }
